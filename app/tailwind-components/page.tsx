dab03bc9a1f586dd9747187e06d7f2f82b4ed1bd
HajiAli <senayousofiali@gmail.com>
2024-08-18 09:29:01 +0430

    added: new libs

--- app/tailwind-components/page.tsx
+++ app/tailwind-components/page.tsx
@@ -2,17 +2,15 @@ import ComponentCard from '@/components/Global/ComponentsCard'
 import React from 'react'
 import { IoHeartDislikeCircleOutline } from 'react-icons/io5'
 import { FaRegAddressCard } from "react-icons/fa6";
+import CollectionList from '@/components/CollectionList/CollectionList';
+import componentsData from '../../data/componentsData'
 
 
 export default function page () {
 
     return (
         <div className="flex flex-wrap mt-20 justify-center gap-4 w-[70%] mx-auto">
-            <ComponentCard 
-                title='Card Sections' 
-                icon={<FaRegAddressCard size={40} />} 
-                description='10 Components'
-            />
+            <CollectionList componentsData={componentsData}/>
         </div>
 
     )
